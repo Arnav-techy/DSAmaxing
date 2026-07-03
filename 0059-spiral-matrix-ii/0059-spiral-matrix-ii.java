72c79a463aa8f06ba3de33f7f864ad52b6e03113
Arnav Mishra <arnavmishraprof2005@gmail.com>
2026-07-03 11:30:34 +0530

    Time: 0 ms (100%), Space: 42.7 MB (90.27%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,71 +1,34 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int [][] a=new int [n][n];
-        int up=0; 
-        int down=1; 
-        int right=2; 
-        int left=3;
+        int[][] a=new int[n][n];
+        int top=0;
+        int bottom =n-1;
+        int left=0;
+        int right=n-1;
+        int num=1;
 
-        int up_wall=0;
-        int right_wall=n;
-        int left_wall=-1;
-        int down_wall=n;
-
-        int direction=right;
-        int ans=1;
-        int i=0;
-        int j=0;
-
-        while(ans<=n*n){
-           
-            if (direction==right){
-                while(j<right_wall){
-                    a[i][j]=ans;
-                    ans++;
-                    j++;
-                }
-                i=i+1;
-                j=j-1;
-                right_wall-=1;
-                direction=down;
+        while(left<=right&&top<=bottom){
+            for(int i=left;i<=right;i++){
+                a[top][i]=num++;
             }
-            else if(direction==down){
-                while(i<down_wall){
-                    a[i][j]=ans;
-                    ans++;
-                    i++;
-                }
-                i-=1;
-                j-=1;
-                down_wall-=1;
-                direction=left;
+            top++;
+            for(int i=top;i<=bottom;i++){
+                a[i][right]=num++;
             }
-            else if(direction==left){
-                while(j>left_wall){
-                    a[i][j]=ans;
-                    ans++;
-                    j--;
-                }
-                i-=1;
-                j+=1;
-                left_wall+=1;
-                direction=up;
+            right--;
+            if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                a[bottom][i]=num++;
             }
-            else if(direction==up){
-                while(i>up_wall){
-                    a[i][j]=ans;
-                    ans++;
-                    i--;
-                }
-                i+=1;
-                j+=1;
-                up_wall+=1;
-                direction=right;
+            bottom--;
+            }
+            if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                a[i][left]=num++;
+            }
+            left++;
             }
         }
+        
         return a;
-
-        
-        
-    }
-}
+}}
