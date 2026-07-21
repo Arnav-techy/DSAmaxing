514a8c4521c40cf7602bcb613ac6d9d32561c20c
Arnav Mishra <arnavmishraprof2005@gmail.com>
2026-07-22 00:10:22 +0530

    Time: 1 ms (89.81%), Space: 42.8 MB (15.22%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-       if ((n > 0 && 1073741824 % n == 0 && n % 3 == 1) || n == 1)
-       return true;
-       else
-       return false;
-        
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
     }
 }
